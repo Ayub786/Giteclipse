e57b2095218f857f38b1d52f408eb3a1813c3109
Ayub786 <ayubabc044@gmail.com>
2019-05-30 05:32:26 -0700

    model

---
+++ Demo/src/DemoGit.java
@@ -0,0 +1,22 @@
+
+public class DemoGit {
+private int sal;
+private String wef;
+public int getSal() {
+	return sal;
+}
+public void setSal(int sal) {
+	this.sal = sal;
+}
+public String getWef() {
+	return wef;
+}
+public void setWef(String wef) {
+	this.wef = wef;
+}
+public DemoGit(int sal, String wef) {
+	super();
+	this.sal = sal;
+	this.wef = wef;
+}
+}
